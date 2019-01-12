8572e83f65ec9a0a34e9b3311eb2e0ff0353b810
Daniel Everland <danieleverland@gmail.com>
2019-01-12 18:54:21 +0100

    init

---
+++ Lil'Dan/Program.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace Lil_Dan
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string token = args[0];
+            new Program().MainAsync(token).GetAwaiter().GetResult();
+        }
+             
+
+        public async Task MainAsync(string token)
+        {
+            var client = new DiscordSocketClient();
+
+            client.Log += Log;
+            
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
